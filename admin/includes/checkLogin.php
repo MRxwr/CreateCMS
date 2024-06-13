2aacd673f851db9432de4d61ddfb487838e8edd4
MRxwr <nasserhatab@gmail.com>
2024-06-14 02:57:06 +0300

    Update checkLogin.php

--- admin/includes/checkLogin.php
+++ admin/includes/checkLogin.php
@@ -12,6 +12,9 @@ if ( isset($_COOKIE["cmsCreate"]) && !empty($_COOKIE["cmsCreate"]) ){
 		date_default_timezone_set('Asia/Kuwait');
 		$date = date('Y-m-d H:i:s');
 		$userType = 1;
+	}else{
+		setcookie("cmsCreate", "", time() - 3600, '/');
+		header('LOCATION: login.php?error=5');die();
 	}
 }elseif( isset($_GET["page"]) && $_GET["page"] == "logout" ){
 	setcookie("cmsCreate", "", time() - 3600, '/');
