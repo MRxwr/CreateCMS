38ef0dc304b0b0304a69e38b3e120a28a919eafe
MRxwr <nasserhatab@gmail.com>
2024-06-14 01:38:33 +0300

    Update checkLogin.php

--- admin/includes/checkLogin.php
+++ admin/includes/checkLogin.php
@@ -3,26 +3,21 @@ if ( !$_COOKIE["cmsCreate"] || ( isset( $_GET["page"] ) && $_GET["page"] == "log
 	setcookie("cmsCreate", "", time() - 3600, '/');
 	header('LOCATION: login.php');
 }else{
-	$sql = "SELECT *
-			FROM `user`
-			WHERE 
-			`hash` LIKE '".$_COOKIE["cmsCreate"]."'
-			";
-	$result = $dbconnect->query($sql);
-	$userType = 0;
-	if ( $result->num_rows < 1 ){
-		$sql = "SELECT *
-				FROM `employee`
-				WHERE 
-				`hash` LIKE '".$_COOKIE["cmsCreate"]."'
-				";
-		$result = $dbconnect->query($sql);
+	if( $user = selectDBNew("user",[$_COOKIE["cmsCreate"]],"`hash` LIKE ? AND `status` = '0''","") ){
+		$userId = $user[0]["id"];
+		$username = $user[0]["username"];
+		date_default_timezone_set('Asia/Kuwait');
+		$date = date('Y-m-d H:i:s');
+		$userType = 0;
+	}elseif( $user = selectDBNew("employee",[$_COOKIE["cmsCreate"]],"`hash` LIKE ? AND `status` = '0''","") ){
+		$userId = $user[0]["id"];
+		$username = $user[0]["username"];
+		date_default_timezone_set('Asia/Kuwait');
+		$date = date('Y-m-d H:i:s');
 		$userType = 1;
+	}else{
+		setcookie("cmsCreate", "", time() - 3600, '/');
+		header('LOCATION: login.php');
 	}
-	$row = $result->fetch_assoc();
-	$userId = $row["id"];
-	$username = $row["username"];
-	date_default_timezone_set('Asia/Kuwait');
-	$date = date('Y-m-d H:i:s');
 }
 ?>
