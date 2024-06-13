5fb98fb2880c6f6f46bc5a1f4acf806efddc0c75
MRxwr <nasserhatab@gmail.com>
2024-06-14 02:13:06 +0300

    Update checkLogin.php

--- admin/includes/checkLogin.php
+++ admin/includes/checkLogin.php
@@ -1,6 +1,6 @@
 <?php
-require_once('includes/config.php');
-require_once('includes/functions.php');
+require_once('config.php');
+require_once('functions.php');
 if ( isset($_COOKIE["cmsCreate"]) && !empty($_COOKIE["cmsCreate"]) ){
 	if( $user = selectDBNew("user",[$_COOKIE["cmsCreate"]],"`hash` LIKE ? AND `status` = '0'","") ){
 		$userId = $user[0]["id"];
@@ -16,24 +16,24 @@ if ( isset($_COOKIE["cmsCreate"]) && !empty($_COOKIE["cmsCreate"]) ){
 		$userType = 1;
 	}else{
 		setcookie("cmsCreate", "", time() - 3600, '/');
-		header('LOCATION: login.php?error=3');die();
+		header('LOCATION: ../login.php?error=3');die();
 	}
 }elseif( isset($_GET["page"]) && $_GET["page"] == "logout" ){
 	setcookie("cmsCreate", "", time() - 3600, '/');
-	header('LOCATION: login.php?error=2');die();
+	header('LOCATION: ../login.php?error=2');die();
 }elseif( isset($_POST["username"]) && !empty($_POST["username"]) && isset($_POST["password"]) && !empty($_POST["password"]) ){
 	if( $user = selectDBNew("user",[$_POST['username'],sha1($_POST['password'])],"`username` LIKE ? AND `password` LIKE ? AND `status` = '0'","") ){
 		setcookie('cmsCreate', md5(time().$_POST['username']), time() + (3600*24*30) , '/');
 		updateDB("user",["hash"=>md5(time().$_POST['username'])],"`id` = {$user[0]["id"]}");
 		$error = 0;
-		header('LOCATION: index.php');die();
+		header('LOCATION: ../index.php');die();
 	}elseif( $user = selectDBNew("employee",[$_POST['username'],sha1($_POST['password'])],"`username` LIKE ? AND `password` LIKE ? AND `status` = '0'","") ){
 		setcookie('cmsCreate', md5(time().$_POST['username']), time() + (3600*24*30) , '/');
 		updateDB("employee",["hash"=>md5(time().$_POST['username'])],"`id` = {$user[0]["id"]}");
 		$error = 0;
-		header("LOCATION: index.php?page=details&action=employees&id={$user[0]["id"]}");die();
+		header("LOCATION: ../index.php?page=details&action=employees&id={$user[0]["id"]}");die();
 	}else{
-		header('LOCATION: login.php?error=1');die();
+		header('LOCATION: ../login.php?error=1');die();
 	}
 }
 ?>
