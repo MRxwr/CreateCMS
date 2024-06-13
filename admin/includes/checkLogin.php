de49c71ce8aabb67ee94dca43c1e703fa6061ad9
MRxwr <nasserhatab@gmail.com>
2024-06-14 01:53:56 +0300

    Update checkLogin.php

--- admin/includes/checkLogin.php
+++ admin/includes/checkLogin.php
@@ -3,13 +3,13 @@ if ( !$_COOKIE["cmsCreate"] || ( isset( $_GET["page"] ) && $_GET["page"] == "log
 	setcookie("cmsCreate", "", time() - 3600, '/');
 	header('LOCATION: login.php');
 }else{
-	if( $user = selectDBNew("user",[$_COOKIE["cmsCreate"]],"`hash` LIKE ? AND `status` = '0''","") ){
+	if( $user = selectDBNew("user",[$_COOKIE["cmsCreate"]],"`hash` LIKE ? AND `status` = '0'","") ){
 		$userId = $user[0]["id"];
 		$username = $user[0]["username"];
 		date_default_timezone_set('Asia/Kuwait');
 		$date = date('Y-m-d H:i:s');
 		$userType = 0;
-	}elseif( $user = selectDBNew("employee",[$_COOKIE["cmsCreate"]],"`hash` LIKE ? AND `status` = '0''","") ){
+	}elseif( $user = selectDBNew("employee",[$_COOKIE["cmsCreate"]],"`hash` LIKE ? AND `status` = '0'","") ){
 		$userId = $user[0]["id"];
 		$username = $user[0]["username"];
 		date_default_timezone_set('Asia/Kuwait');
